532ba1a7edd3e4af5c5fc831bfc7549e8f63afb7
Oleksandr Manzyuk <manzyuk@googlemail.com>
2011-01-25 19:41:39 +0000

    Improved the parser by re-factoring the grammar and getting rid of `try'.

--- Untyped.hs
+++ Untyped.hs
@@ -9,9 +9,9 @@ import System.IO
 type Name = String
 
 data Term = Var Name
-          | Abs Name Term
-          | App Term Term
-            deriving (Show, Eq)
+	  | Abs Name Term
+	  | App Term Term
+	    deriving (Show, Eq)
 
 -- Lexer
 
@@ -21,11 +21,11 @@ scan :: String -> Either ParseError [Token]
 scan = parse toks "(stdin)"
 
 tok  = liftA2 (,) getPosition $ choice [ many1 letter
-                                       , string "("
-                                       , string ")"
-                                       , string "."
-                                       , string "\\"
-                                       ]
+				       , string "("
+				       , string ")"
+				       , string "."
+				       , string "\\"
+				       ]
 
 toks = spaces *> many (tok <* spaces) <* eof
 
@@ -48,13 +48,8 @@ paren = between (symb "(") (symb ")")
 
 p_var = Var <$> name
 p_abs = liftA2 Abs (symb "\\" *> name <* symb ".") p_term
-p_app = foldl1 App <$> many2 (p_var <|> paren p_term)
-    where many2 p = liftA2 (:) p (many1 p)
 
-p_term = choice [ try p_app
-                , p_abs
-                , p_var
-                ] <|> paren p_term
+p_term = p_abs <|> (foldl1 App <$> many1 (p_var <|> paren p_term))
 
 p_program = p_term <* eof
 
@@ -74,8 +69,8 @@ extend s v t = \u -> if u == v then t else s u
 successor :: Name -> Name
 successor = reverse . successor' . reverse
     where successor' [] = "A"
-          successor' (c:cs) | c < 'z'   = (succ c) : cs
-                            | otherwise = 'A' : successor cs
+	  successor' (c:cs) | c < 'z'   = (succ c) : cs
+			    | otherwise = 'A' : successor cs
 
 -- An infinite lexicographically ordered list of names.
 names :: [Name]
@@ -83,15 +78,15 @@ names = iterate successor "A"
 
 freshName :: Name -> Set.Set Name -> Name
 freshName n s | n `Set.member` s = head $ dropWhile (`Set.member` s) names
-              | otherwise        = n
+	      | otherwise        = n
 
 substitute :: Term -> Substitution -> Term
 substitute (Var v) s     = s v
 substitute (Abs v e) s   = Abs v' (substitute e s')
     where vs = Set.unions [ freeVars (s w) |
-                            w <- Set.toList . Set.delete v . freeVars $ e]
-          v' = freshName v vs
-          s' = extend s v (Var v')
+			    w <- Set.toList . Set.delete v . freeVars $ e]
+	  v' = freshName v vs
+	  s' = extend s v (Var v')
 substitute (App e1 e2) s = App (substitute e1 s) (substitute e2 s)
 
 evalOne :: Term -> Maybe Term
@@ -107,9 +102,9 @@ ppTerm (Var v)    = v
 ppTerm (Abs v e)  = "\\" ++ v ++ " . " ++ ppTerm e
 ppTerm (App e e') = (pp e) ++ " " ++ (pp e')
     where pp t@(Var v)    =          ppTerm t
-          pp t@(Abs v e)  = parens $ ppTerm t
-          pp t@(App e e') = parens $ ppTerm t
-          parens text = "(" ++ text ++ ")"
+	  pp t@(Abs v e)  = parens $ ppTerm t
+	  pp t@(App e e') = parens $ ppTerm t
+	  parens text = "(" ++ text ++ ")"
 
 -- Interpreter
 
@@ -122,12 +117,12 @@ main = do
   forever repl
     where
       repl = do
-        putStr prompt
-        input <- getLine
-        case scan input of
-          Left  err  -> putStrLn "Parse error:" >> print err
-          Right toks -> case toks of
-                          []      -> return ()
-                          (tok:_) -> case parse ((setPosition . fst $ tok) >> p_program) "" toks of
-                                       Left  err  -> putStrLn "Parse error:" >> print err
-                                       Right term -> putStrLn . ppTerm $ eval term
+	putStr prompt
+	input <- getLine
+	case scan input of
+	  Left  err  -> putStrLn "Parse error:" >> print err
+	  Right toks -> case toks of
+			  []      -> return ()
+			  (tok:_) -> case parse ((setPosition . fst $ tok) >> p_program) "" toks of
+				       Left  err  -> putStrLn "Parse error:" >> print err
+				       Right term -> putStrLn . ppTerm $ eval term
